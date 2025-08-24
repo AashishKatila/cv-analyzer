908c7d6a8e3e2a165b6d032b6df974994955bc02
aashishKatila <aashishkatila@gmail.com>
2025-08-24 11:29:19 +0545

    fix: CORS issue

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://cv-analyzer-psi.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
